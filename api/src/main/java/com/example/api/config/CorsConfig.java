be90df3abbc3299d2dad1eb0028a40270c06520a
Alice Arruda Atalla <aliceatalla36@gmail.com>
2024-04-04 13:09:08 -0300

    feature(api): add allowed origin urls

--- api/src/main/java/com/example/api/config/CorsConfig.java
+++ api/src/main/java/com/example/api/config/CorsConfig.java
@@ -16,6 +16,8 @@ public class CorsConfig {
         config.setAllowCredentials(true);
 
         config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("https://pokedex-pi-plum.vercel.app");
+        config.addAllowedOrigin("https://pokedex-git-main-aliceaatallas-projects.vercel.app/");
 
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
